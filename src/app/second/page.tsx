e7a0bd75ac75c17a0d869e91bcf8ab20321c840d
zach grindle <elizabethgodsey@Elizabeths-MacBook-Pro-2.local>
2025-08-26 18:36:59 -0500

    got second page workjing 30 minutes logged

---
+++ src/app/second/page.tsx
@@ -0,0 +1,13 @@
+// app/second/page.tsx
+export default function SecondPage() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-2xl">
+
+      <p>This is the second page</p>
+        <p>
+          my beeb
+        </p>
+    </div>
+    
+  );
+}
--- src/app/second/second.tsx
+++
@@ -1,3 +0,0 @@
-export default function SecondPage() {
-  return <h1>Welcome to the Second Page</h1>;
-}
