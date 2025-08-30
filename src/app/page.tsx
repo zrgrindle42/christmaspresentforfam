e37a0d0ac3ff549044301b82dac86b1009393e19
zach grindle <elizabethgodsey@Elizabeths-MacBook-Pro-2.local>
2025-08-30 16:02:50 -0500

    refactored some of the layout.tsx into separate css + init of username box 2 hr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+"use client";
 import Image from "next/image";
 import Link  from "next/link";
+import './page.css';
+import { useState } from 'react';
+//import TextField from '@mui/material/TextField';
 
+//maybe import a css file
+//export default function for global styles in this class
 export default function Home() {
+  // ts logic goes here for login area
+  const[username, setName] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+
+  };
+
+
+  
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="layout-grid">
+      <main className="main-content">
+       
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -13,43 +30,25 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <div className = "login-field">
+            <input
+              type="text"
+              value={username}
+              onChange={handleChange}
+              placeholder="username"
 
-         <div className="flex gap-4 items-center flex-col sm:flex-row">
+            />
+
+
+
+
+        </div>
+      
+      <div className="login-button-class">
           <Link href="/second"
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            //href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            // target="_blank"
-            // rel="noopener noreferrer"
-          >
-            {/* <Image
-              className="dark:invert"
-              src="/vercel.svg" // pic of my beeb here
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            /> */}
+            className="login-button">
             Login
           </Link> 
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
